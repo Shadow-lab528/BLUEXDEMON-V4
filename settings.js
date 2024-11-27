9859da0ad7eeb1054059f774cec41260580f0302
Shadow-lab528 <masterlegendary063@gmail.com>
2024-11-27 02:37:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-global.pairingNumber = "2347041039367"
+global.pairingNumber = "2348169546065"
  
-global.botNumber = ["2347041039367"]
+global.botNumber = ["2348169546065"]
 
 global.sign = "✘"
 // ☞ ➻ ➽ ☛ ✔
